af27a2c2a7d7093bdcb147819503e729506dee5d
leeclaire156 <lee.claire156@gmail.com>
2023-01-16 19:18:28 -0500

    Adds README framework to generate README markdown file

--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown')
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -9,62 +10,62 @@ const questions = [
         message: "What is the name of your file?",
         name: "fileName",
     },
-    // {
-    //     type: "input",
-    //     message: "What is the name of your project?",
-    //     name: "title",
-    // },
-    // {
-    //     type: "input",
-    //     message: "How would you describe your project",
-    //     name: "description",
-    // },
-    // {
-    //     type: "input",
-    //     message: "How do you install your project?",
-    //     name: "installation",
-    // },
-    // {
-    //     type: "input",
-    //     message: "How do you use your project?",
-    //     name: "usage",
-    // },
-    // {
-    //     type: "input",
-    //     message: "How would another person contribute to your project?",
-    //     name: "contributing",
-    // },
-    // {
-    //     type: "input",
-    //     message: "What tests can be run?",
-    //     name: "tests",
-    // },
-    // {
-    //     type: "list",
-    //     message: "Which license does your project have?",
-    //     choices: ["GNU AGPLv3", "GNU GPLv3", "GNU LGPLv3",
-    //         "Mozilla Public License 2.0", "Apache License 2.0",
-    //         "MIT License", "Boost Software License 1.0", "The Unlicense",
-    //         "CC0-1.0", "CC-BY-4.0", "CC-BY-SA-4.0", "SIL Open Font License 1.1",
-    //         "CERN-OHL-P-2.0", "CERN-OHL-W-2.0", "CERN-OHL-S-2.0", "Other", "None"],
-    //     name: "license",
-    // },
-    // {
-    //     type: "input",
-    //     message: "What is your GitHub username?",
-    //     name: "github",
-    // },
-    // {
-    //     type: "input",
-    //     message: "What is your email?",
-    //     name: "email",
-    // },
+    {
+        type: "input",
+        message: "What is the name of your project?",
+        name: "title",
+    },
+    {
+        type: "input",
+        message: "How would you describe your project",
+        name: "description",
+    },
+    {
+        type: "input",
+        message: "How do you install your project?",
+        name: "installation",
+    },
+    {
+        type: "input",
+        message: "How do you use your project?",
+        name: "usage",
+    },
+    {
+        type: "input",
+        message: "How would another person contribute to your project?",
+        name: "contributing",
+    },
+    {
+        type: "input",
+        message: "How can tests can be run? Please provide testing instructions.",
+        name: "tests",
+    },
+    {
+        type: "list",
+        message: "Which license does your project have?",
+        choices: ["GNU AGPLv3", "GNU GPLv3", "GNU LGPLv3",
+            "Mozilla Public License 2.0", "Apache License 2.0",
+            "MIT License", "Boost Software License 1.0", "The Unlicense",
+            "CC0-1.0", "CC-BY-4.0", "CC-BY-SA-4.0", "SIL Open Font License 1.1",
+            "CERN-OHL-P-2.0", "CERN-OHL-W-2.0", "CERN-OHL-S-2.0", "Other", "None"],
+        name: "license",
+    },
+    {
+        type: "input",
+        message: "What is your GitHub username?",
+        name: "github",
+    },
+    {
+        type: "input",
+        message: "What is your email?",
+        name: "email",
+    },
 ];
 
 
 // TODO: Create a function to write README file
 function writeToFile(data) {
-    const readmeText = `hello. ${data.fileName}`;
+    const readmeText = generateMarkdown(data);
     console.log(readmeText);
     fs.writeFile(`${data.fileName}.md`, readmeText, (err) => err ? console.error(err) : console.log(`${data.fileName}.md created!`));
 }
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -2,20 +2,56 @@
 // WHEN I choose a license for my application from a list of options
 // THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) { }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) { }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) { }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  
+  ## Description
 
+  ${data.description}
+  
+
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [How to contribute](#how-to-contribute)
+  - [License](#license)
+  - [Test Instructions](#test-instructions)
+  - [Questions](#questions)
+
+  ## Installation
+
+  ${data.installation}
+
+  ## Usage
+  
+  ${data.usage}
+  
+  ## How to contribute
+  
+  ${data.contributing}
+  
+  ## License
+  
+  ${data.license}
+
+  ## Test Instructions
+
+  ${data.tests}
+    
+  ## Questions
+  
+  If you have any questions, my GitHub profile is [www.github.com/${data.github}](www.github.com/${data.github}), and my email is [${data.email}](mailto:${data.email}).
 `;
 }
 
